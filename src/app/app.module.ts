2bfa618a837c7e3b51dfe7119e4001da7d724195
Vitória Ketillin Fusco <vitoriaketillin@hotmail.com>
2021-11-14 14:56:27 -0300

    v1.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,15 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { QRScanner } from '@ionic-native/qr-scanner/ngx';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    QRScanner],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
 
+import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner/ngx';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  scanner: any;
+  content: HTMLElement;
+  resultado = '';
 
-  constructor() {}
+  constructor(private qrScanner: QRScanner) {}
 
+  lerQRCode() {
+    this.qrScanner.prepare()
+      .then((status: QRScannerStatus) => {
+        if (status.authorized) {
+          // camera permission was granted
+          this.content = document.getElementsByTagName('ion-content')[0];
+          this.content.style.opacity = '0';
+
+          // start scanning
+          this.scanner = this.qrScanner.scan().subscribe((text: string) => {
+            console.log('Scanned', text);
+
+            this.content.style.opacity = '1';
+
+            this.qrScanner.hide(); // hide camera preview
+            this.scanner.unsubscribe(); // stop scanning
+          });
+        } else if (status.denied) {
+          alert('negado');
+        } else {
+          alert('negado, tente novamente');
+        }
+      })
+      .catch((e: any) => console.log('Error is', e));
+  }
 }
